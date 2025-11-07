3588983e475fce2fd4d64a180478c68dc62f680f
Hien-Ly <151255536+H13NL@users.noreply.github.com>
2025-11-07 06:45:22 +0000

    MancalaPit

---
+++ src/MancalaPit.java
@@ -0,0 +1,24 @@
+public class MancalaPit {
+    protected int stoneNum;
+    protected boolean isPlayerAPit;
+    protected boolean isStore;
+
+    public MancalaPit(boolean ownerIsA, boolean isStore) {
+        this.ownerIsA = ownerIsA;
+        this.isStore = isStore;
+    }
+
+    public int getStoneNum() { 
+        return stoneNum; 
+    }
+    public void setStoneNum(int stoneNum) {
+        this.stoneNum = stoneNum; 
+    }
+
+    public boolean isPlayerAPit() { 
+        return ownerIsA; 
+    }
+    public boolean isStore() { 
+        return isStore; 
+    }
+}
